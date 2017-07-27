c67ea80af15ecb6a9c2053bbdb83c12879c7d66c
Alex <alex@Alexs-MBP.wp.comcast.net>
2017-07-27 17:22:11 -0400

    done

---
+++ main.css
@@ -0,0 +1,53 @@
+app.controller('MainController', ['$scope', function($scope) {
+  $scope.today = new Date();
+
+  $scope.appetizers = [
+    {
+      name: 'Caprese',
+      description: 'Mozzarella, tomatoes, basil, balsmaic glaze.',
+      price: 4.95
+    },
+    {
+      name: 'Mozzarella Sticks',
+      description: 'Served with marinara sauce.',
+      price: 3.95
+    },
+    {
+      name: 'Bruschetta',
+      description: 'Grilled bread garlic, tomatoes, olive oil.',
+      price: 4.95
+    }
+  ];
+	$scope.mains = [
+    {
+      name: 'Roti Canai',
+      description: '印度面包.',
+      price: 4
+    },
+    {
+      name: 'Pad Thai',
+      description: 'Yummy Thai-styled fried noodle.',
+      price: 10
+    },
+    {
+      name: 'Tom Yum Soup',
+      description: 'Delicious Soup.',
+      price: 5
+    }];
+  $scope.extras = [
+    {
+      name: 'White Rice',
+      description: 'Homemade organic white rice.',
+      price: 1
+    },
+    {
+      name: 'Soda',
+      description: 'Canned soda.',
+      price: 2
+    },
+    {
+      name: 'Fried Ice Cream',
+      description: 'Choice of Vanilla, Chocolate, and Strawberry.',
+      price: 3.5
+    }];
+}]);
